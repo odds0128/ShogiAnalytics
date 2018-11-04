d39297d7a55fd578387a27cfbd04d4d87f299929
Ryoya Funato <r.funato@Nuviana.local>
2018-11-04 16:08:15 +0900

    headerの高さの調節についての部分

---
+++ src/components/myHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id='header'>myHeader</div>
+</template>
+
+<script>
+const header_height = 60
+
+document.addEventListener("DOMContentLoaded", function(){
+    const css_header = document.getElementById('header');
+    css_header.style.height = header_height + 'px';
+});
+
+export default {
+    name: 'myHeader',
+    data: function(){
+        return {
+            header_height: header_height
+        }
+    }
+}
+</script>
+
+<style>
+    #header{
+        background: #00ff00;
+        width: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+</style>
---
+++ src/pages/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <my-header id="header"/>
+    <div id="body">
+      <img src="@/assets/japanese-chess/piece/60x64/sOu.png">
+    </div>
+  </div>
+</template>
+
+<script>
+import myHeader from "../components/myHeader.vue";
+
+window.onload = function(){
+  const header_height = document.getElementById('header').clientHeight;
+  console.log(header_height);
+  const css_body = document.getElementById('body')
+  css_body.style.marginTop = header_height + 20 + 'px';
+};
+
+export default {
+  name: "App",
+  components: {
+    myHeader
+  }
+};
+</script>
+
+<style>
+#body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
